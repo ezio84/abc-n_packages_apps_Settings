ec45ae7b60187dbb54979958d6503bf2e9613a28
ezio84 <brabus84@gmail.com>
2017-02-03 00:26:51 +0100

    Allow to quickly open Running Services from QS panel [2/2]

--- src/com/android/settings/Settings.java
+++ src/com/android/settings/Settings.java
@@ -177,4 +177,6 @@ public class Settings extends SettingsActivity {
     public static class AccountDashboardActivity extends SettingsActivity {}
     public static class SystemDashboardActivity extends SettingsActivity {}
 
+    // Custom additions.
+    public static class DevRunningServicesActivity extends SettingsActivity { /* empty */ }
 }
