60290743d3076b84a3be5fa8a839f7022ad574b3
Giuseppe Maggio <jertlok@protonmail.com>
2019-12-15 10:28:46 -0500

    Initial import for Pixel-specific feature Settings
    Change-Id: If9a87439e03be388de0e5b9059dc93c77243062d

--- Android.mk
+++ Android.mk
@@ -21,6 +21,7 @@ LOCAL_MODULE_TAGS := optional
 LOCAL_USE_AAPT2 := true
 
 LOCAL_SRC_FILES := $(call all-java-files-under, src)
+LOCAL_SRC_FILES += $(call all-java-files-under, ../../../external/google/SettingsGoogle/src)
 
 LOCAL_STATIC_ANDROID_LIBRARIES := \
     androidx-constraintlayout_constraintlayout \
@@ -52,6 +53,9 @@ LOCAL_STATIC_JAVA_LIBRARIES := \
     contextualcards \
     settings-logtags \
     zxing-core-1.7
+
+LOCAL_RESOURCE_DIR := $(LOCAL_PATH)/res \
+    external/google/SettingsGoogle/res
 
 LOCAL_PROGUARD_FLAG_FILES := proguard.flags
 
